888f2ef092f59cdfcc80f22660421e8cf1cf5d05
Tuan Dau <tukauu@gmail.com>
2021-09-23 17:07:50 -0400

    Turned geocoding API call into a python function

--- backend/geocoding.py
+++ backend/geocoding.py
@@ -2,23 +2,22 @@ import requests
 
 API_KEY = 'AIzaSyCqmlVrzMvQZHDK1VqOeoap7mUOFq1dhVc'
 
-# def findLatLng(address):
-address = "Toronto, ON"
+def findLatLng(address):
 
-params = {
-    'key': API_KEY,
-    'address': address
-}
+    params = {
+        'key': API_KEY,
+        'address': address
+    }
 
-base_url = 'https://maps.googleapis.com/maps/api/geocode/json?'
+    base_url = 'https://maps.googleapis.com/maps/api/geocode/json?'
 
-response = requests.get(base_url, params=params).json()
+    response = requests.get(base_url, params=params).json()
 
-# Check if the request was processed successfully
-if response['status'] == 'OK':
-    # If so, grab the long lat from the response
-    geometry = response['results'][0]['geometry']    
-    latitude = geometry['location']['lat']
-    longitude = geometry['location']['lng']
+    # Check if the request was processed successfully
+    if response['status'] == 'OK':
+        # If so, grab the lat and long from the response
+        geometry = response['results'][0]['geometry']    
+        latitude = geometry['location']['lat']
+        longitude = geometry['location']['lng']
 
-print(latitude, longitude)
+    return [latitude, longitude]
