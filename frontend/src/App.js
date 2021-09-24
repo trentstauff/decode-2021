347cbf044cfe87488579a4c756b00b5db7fb56f2
ksondjaja <sondjajaja@gmail.com>
2021-09-24 00:52:48 -0400

    update arcs every second

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,68 +8,76 @@ import './App.css';
 
 export class App extends React.Component {
 
-  state = {
-    arcsData:
-      [{
-        startLat: (Math.random() - 0.5) * 180,
-        startLng: (Math.random() - 0.5) * 360,
-        endLat: (Math.random() - 0.5) * 180,
-        endLng: (Math.random() - 0.5) * 360,
-        color: ['#faac6e', '#a89a50', '#61804c', '#2a604b', '#0c3e3e','#0c2a2d'][Math.floor(Math.random()*5)],
-        stroke: Math.random()*(1.5-0.2)
-      },
-      {
-        startLat: (Math.random() - 0.5) * 180,
-        startLng: (Math.random() - 0.5) * 360,
-        endLat: (Math.random() - 0.5) * 180,
-        endLng: (Math.random() - 0.5) * 360,
-        color: ['#faac6e', '#a89a50', '#61804c', '#2a604b', '#0c3e3e','#0c2a2d'][Math.floor(Math.random()*5)],
-        stroke: Math.random()*(1.5-0.2)
-      },
-      {
-        startLat: (Math.random() - 0.5) * 180,
-        startLng: (Math.random() - 0.5) * 360,
-        endLat: (Math.random() - 0.5) * 180,
-        endLng: (Math.random() - 0.5) * 360,
-        color: ['#faac6e', '#a89a50', '#61804c', '#2a604b', '#0c3e3e','#0c2a2d'][Math.floor(Math.random()*5)],
-        stroke: Math.random()*(1.5-0.2)
-      },
-      {
-        startLat: (Math.random() - 0.5) * 180,
-        startLng: (Math.random() - 0.5) * 360,
-        endLat: (Math.random() - 0.5) * 180,
-        endLng: (Math.random() - 0.5) * 360,
-        color: ['#faac6e', '#a89a50', '#61804c', '#2a604b', '#0c3e3e','#0c2a2d'][Math.floor(Math.random()*5)],
-        stroke: Math.random()*(1.5-0.2)
-      },
-      {
-        startLat: (Math.random() - 0.5) * 180,
-        startLng: (Math.random() - 0.5) * 360,
-        endLat: (Math.random() - 0.5) * 180,
-        endLng: (Math.random() - 0.5) * 360,
-        color: ['#faac6e', '#a89a50', '#61804c', '#2a604b', '#0c3e3e','#0c2a2d'][Math.floor(Math.random()*5)],
-        stroke: Math.random()*(1.5-0.2)
-      },
-      {
-        startLat: (Math.random() - 0.5) * 180,
-        startLng: (Math.random() - 0.5) * 360,
-        endLat: (Math.random() - 0.5) * 180,
-        endLng: (Math.random() - 0.5) * 360,
-        color: ['#faac6e', '#c3a156', '#8f924d', '#61804c', '#165556', '#0c2a2d'][Math.floor(Math.random()*5)],
-        stroke: Math.random()*(1.5-0.2)
-      },
-    ],
-  };
-
-  // changeArcs(arcs){
-  //   this.setState({arcsData: arcs})
+  // state={
+  //   arcsData:[
+  //     {
+  //       startLat: (Math.random() - 0.5) * 180,
+  //       startLng: (Math.random() - 0.5) * 360,
+  //       endLat: (Math.random() - 0.5) * 180,
+  //       endLng: (Math.random() - 0.5) * 360,
+  //       color: ['#faac6e', '#c3a156', '#8f924d', '#61804c', '#165556', '#0c2a2d'][Math.floor(Math.random()*5)],
+  //       stroke: Math.random()*(1.5-0.2)
+  //     },
+  //     {
+  //       startLat: (Math.random() - 0.5) * 180,
+  //       startLng: (Math.random() - 0.5) * 360,
+  //       endLat: (Math.random() - 0.5) * 180,
+  //       endLng: (Math.random() - 0.5) * 360,
+  //       color: ['#faac6e', '#c3a156', '#8f924d', '#61804c', '#165556', '#0c2a2d'][Math.floor(Math.random()*5)],
+  //       stroke: Math.random()*(1.5-0.2)
+  //     },
+  //     {
+  //       startLat: (Math.random() - 0.5) * 180,
+  //       startLng: (Math.random() - 0.5) * 360,
+  //       endLat: (Math.random() - 0.5) * 180,
+  //       endLng: (Math.random() - 0.5) * 360,
+  //       color: ['#faac6e', '#c3a156', '#8f924d', '#61804c', '#165556', '#0c2a2d'][Math.floor(Math.random()*5)],
+  //       stroke: Math.random()*(1.5-0.2)
+  //     }
+  //   ]
   // }
+
+  
+
+
+  
+  // Updating state with new data mostly works, but now the animation doesn't run
+  // The arcs do update every few seconds because of setInterval inside generateArcs function in World.js
+  // but the arcs themselves stops animating after they're initially drawn
+
+  state = {arcsData: null};
+
+  insertArcs = () => {
+    let arcs = [];
+
+    for(let i=0; i<6; i++){
+      let colors = ['#faac6e', '#c3a156', '#8f924d', '#61804c', '#165556', '#0c2a2d']
+      
+      var arc = {
+        startLat: (Math.random() - 0.5) * 180,
+        startLng: (Math.random() - 0.5) * 360,
+        endLat: (Math.random() - 0.5) * 180,
+        endLng: (Math.random() - 0.5) * 360,
+        color: colors[Math.floor(Math.random()*5)],
+        stroke: Math.random()*(1.5-0.2)
+      };
+
+      arcs.push(arc);
+    }
+
+    return arcs;
+  }
+
+  changeArcs = () => {
+    const arc = this.insertArcs();
+    this.setState({ arcsData: arc });
+  }
 
   render(){
     return(
       <World
       arcsData={this.state.arcsData}
-      // changeArcs={this.changeArcs}
+      changeArcs={this.changeArcs}
       />
     );
   }
--- frontend/src/World.js
+++ frontend/src/World.js
@@ -10,36 +10,43 @@ import background7 from './background7.png';
 
 const World = (props) => {
 
-    const arcsData = props.arcsData;
+  const generateArcs = () => {
+    setInterval(() => props.changeArcs(), 1000);
+  };
 
-    return (<div>
+  window.addEventListener('load', generateArcs);
+
+  const arcsData = props.arcsData;
+
+  return (
+    <div>
         <Globe
-      globeImageUrl={earth}
+          globeImageUrl={earth}
 
-      showGraticules={false}
-      //atmosphereColor={'#83c2db'}
-      atmosphereColor={'white'}
-      atmosphereAltitude={.25}
-      //backgroundColor={'#222222'}
-      backgroundImageUrl={background7}
+          showGraticules={false}
+          //atmosphereColor={'#83c2db'}
+          atmosphereColor={'white'}
+          atmosphereAltitude={.25}
+          //backgroundColor={'#222222'}
+          backgroundImageUrl={background7}
 
-      arcsData={arcsData}
-      arcColor={'color'}
-      arcStroke={'stroke'}
-      arcCircularResolution={10}
-      arcDashLength={.5}
-      arcDashGap={.5}
-      arcDashAnimateTime={3000}
+          arcsData={arcsData}
+          arcColor={'color'}
+          arcStroke={'stroke'}
+          arcCircularResolution={10}
+          arcDashLength={1}
+          arcDashGap={0}
+          arcDashAnimateTime={2000}
 
-      hexPolygonsData={countries.features}
-      hexPolygonResolution={3}
-      hexPolygonMargin={0.3}
-      hexPolygonColor={() => `#${Math.round(Math.random() * Math.pow(2, 24)).toString(16).padStart(6, '0')}`}
-      hexPolygonLabel={({ properties: d }) => `
-        <b>${d.ADMIN} (${d.ISO_A2})</b> <br />
-        Population: <i>${d.POP_EST}</i>
-      `}
-    />
+          hexPolygonsData={countries.features}
+          hexPolygonResolution={3}
+          hexPolygonMargin={0.3}
+          hexPolygonColor={() => `#${Math.round(Math.random() * Math.pow(2, 24)).toString(16).padStart(6, '0')}`}
+          hexPolygonLabel={({ properties: d }) => `
+            <b>${d.ADMIN} (${d.ISO_A2})</b> <br />
+            Population: <i>${d.POP_EST}</i>
+          `}
+        />
     </div>
     );
   };
