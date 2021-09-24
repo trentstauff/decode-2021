9b67d583239ad4d668c20d71d31de9be3cc750ea
Victor Sun <victorjsun@gmail.com>
2021-09-23 20:05:03 -0400

    Fix websocket

--- frontend/src/composables/websockets.tsx
+++ frontend/src/composables/websockets.tsx
@@ -1,16 +1,26 @@
-import { w3cwebsocket as W3CWebSocket } from "websocket";
+// import { w3cwebsocket as W3CWebSocket } from "websocket";
+import io from 'socket.io-client';
 
 const WEBSOCKETHOST = '127.0.0.1';
-const WEBSOCKETPORT = '8000'
+const WEBSOCKETPORT = '5001'
 
 export function initializeWebsocket(setData: (message: any) => void) {
-  const client = new W3CWebSocket('ws://'+WEBSOCKETHOST+':'+WEBSOCKETPORT);
-  client.onopen = () => {
-    console.log('WebSocket Client Connected');
-    client.send("Initialized")
-  };
-  client.onmessage = (message) => {
-    console.log(message);
-    setData(message.data);
-  };
+  const newSocket = io(`http://${WEBSOCKETHOST}:${WEBSOCKETPORT}`);
+  newSocket.on('connect', () => {
+    console.log("connected")
+    // newSocket.send("message")
+  });
+  newSocket.on('message', (data: any) => {
+    console.log(data)
+    setData(data)
+  });
+  // const client = new W3CWebSocket('ws://'+WEBSOCKETHOST+':'+WEBSOCKETPORT);
+  // client.onopen = () => {
+  //   console.log('WebSocket Client Connected');
+  //   client.send("Initialized")
+  // };
+  // client.onmessage = (message) => {
+  //   console.log(message);
+  //   setData(message.data);
+  // };
 }
